f0494bdc7ed2c1e56b5480f26c186753cf612bc4
michaelngosa <ngosamichael@gmail.com>
2022-09-27 14:07:30 -0700

    function that reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-
-"""defining read_file function"""
-
-
-
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-
-    """reads filename with utf-8"""
-
-    with open(filename, encoding='utf-8') as f:
-
-        print(f.read(), end="")
+        """Print the contents of a UTF8 text file to stdout."""
+            with open(filename, encoding="utf-8") as f:
+                        print(f.read(), end="")
