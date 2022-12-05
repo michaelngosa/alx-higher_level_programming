5f8c25d632ea828ce7f501d42a9237ad988b7ea4
michaelngosa <ngosamichael@gmail.com>
2022-12-04 17:15:27 -0800

    My task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_14_usa.
-Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-                                         <mysql password> /<database name>
+   represents city mapper and table
+   Instantiates Base from declarative_base
 """
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
-from model_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+class City(Base):
+    """ Represents City objecs """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
