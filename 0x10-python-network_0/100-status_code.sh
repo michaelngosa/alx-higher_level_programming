0e7dee62d56fe450ba027d33304f6a6d747b20bc
michaelngosa <ngosamichael@gmail.com>
2022-12-15 20:18:36 -0800

    sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,2 +1,3 @@
-cript that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -o /dev/null -I -w "%{http_code}" "$1"
+#!/bin/bash
+# sends a req to $1 URL, display response status code only
+curl -s -o /dev/null -w "%{http_code}" "$1"
