4ae9e7fb708370df2c89e704834f6d0df574eee3
michaelngosa <ngosamichael@gmail.com>
2022-12-17 14:15:11 -0800

    initial commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    response = response.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
