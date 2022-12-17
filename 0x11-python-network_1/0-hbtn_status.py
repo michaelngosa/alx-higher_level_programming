d4d2039a72dfb6e84573784bb5ae1abaa5f71334
michaelngosa <ngosamichael@gmail.com>
2022-12-17 14:41:16 -0800

    Python script that fetches web address

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Prints different results of
-a request
-"""
-import urllib.request
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
 
+from urllib import request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
-        print("-\t type:", type(html))
-        print("-\t content:", html)
-        print("-\t utf8 content:", html.decode("utf-8"))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
