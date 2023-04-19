76c0a4eb53b344cd7228ec7a14b988ac29d71715
Michael ngosa <97114460+michaelngosa@users.noreply.github.com>
2023-04-19 14:05:44 +0200

    Delete 0x13-javascript_objects_scopes_closures directory
    Update Git Projects

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-exports.nbOccurences = function (list, searchElement) {
-  const filteredList = list.filter(item => item === searchElement);
-  return filteredList.length;
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-let trackLogs = 0;
-
-exports.logMe = function (item) {
-  console.log(`${trackLogs}: ${item}`);
-  trackLogs++;
-};
